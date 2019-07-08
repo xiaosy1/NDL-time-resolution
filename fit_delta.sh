1489d5428b2185fc8a9809c41af4ce1cce3da705
Suyu Xiao <1203111345@qq.com>
2019-07-08 13:08:05 +0800

    code

---
+++ fit_delta.sh
@@ -0,0 +1,132 @@
+python python/fit_delta.py root/lgad2/50.root root/lgad2_delta/050_30.root 30 50t
+python python/fit_delta.py root/lgad2/50.root root/lgad2_delta/050_50.root 50 50t
+python python/fit_delta.py root/lgad2/50.root root/lgad2_delta/050_70.root 70 50t
+ 
+python python/fit_delta.py root/lgad2/60.root root/lgad2_delta/060_30.root 30 50t
+python python/fit_delta.py root/lgad2/60.root root/lgad2_delta/060_50.root 50 50t
+python python/fit_delta.py root/lgad2/60.root root/lgad2_delta/060_70.root 70 50t
+                                                                             
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_05.root 5 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_10.root 10 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_20.root 20 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_30.root 30 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_40.root 40 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_50.root 50 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_60.root 60 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_70.root 70 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_80.root 80 50t
+python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_90.root 90 50t
+
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_30_145_2.root 30 50t
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_50_145_2.root 50 50t
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_70_145_2.root 70 50t
+
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_05.root 5 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_10.root 10 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_20.root 20 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_30.root 30 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_40.root 40 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_50.root 50 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_60.root 60 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_70.root 70 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_80.root 80 50t
+python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_90.root 90 50t
+
+python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_30.root 30 50t
+python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_50.root 50 50t
+python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_70.root 70 50t
+
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_05.root 5 50t
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_10.root 10 50t
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_20.root 20 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_30.root 30 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_40.root 40 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_50.root 50 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_60.root 60 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_70.root 70 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_80.root 80 50t 
+python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_90.root 90 50t 
+
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_05.root 5 50t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_10.root 10 50t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20.root 20 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_30.root 30 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_40.root 40 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_50.root 50 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_60.root 60 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_70.root 70 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_80.root 80 50t 
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_90.root 90 50t 
+
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_10.root 20 10t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_20.root 20 20t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_30.root 20 30t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_40.root 20 40t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_50.root 20 50t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_60.root 20 60t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_70.root 20 70t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_80.root 20 80t
+python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_90.root 20 90t
+
+
+python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_30.root 30 50t
+python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_50.root 50 50t 
+python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_70.root 70 50t 
+
+
+
+###   BV 60 50 
+
+python python/fit_delta.py root/lgad1/30.root root/lgad1_delta/030_30.root 30 50t 
+python python/fit_delta.py root/lgad1/30.root root/lgad1_delta/030_50.root 50 50t 
+python python/fit_delta.py root/lgad1/30.root root/lgad1_delta/030_70.root 70 50t 
+
+python python/fit_delta.py root/lgad1/40.root root/lgad1_delta/040_30.root 30 50t
+python python/fit_delta.py root/lgad1/40.root root/lgad1_delta/040_50.root 50 50t 
+python python/fit_delta.py root/lgad1/40.root root/lgad1_delta/040_70.root 70 50t 
+
+python python/fit_delta.py root/lgad1/50.root root/lgad1_delta/050_30.root 30 50t 
+python python/fit_delta.py root/lgad1/50.root root/lgad1_delta/050_50.root 50 50t 
+python python/fit_delta.py root/lgad1/50.root root/lgad1_delta/050_70.root 70 50t 
+
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_05.root 5 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_10.root 10 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_20.root 20 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_30.root 30 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_40.root 40 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_50.root 50 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_60.root 60 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_70.root 70 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_80.root 80 50t 
+python python/fit_delta.py root/lgad1/60.root root/lgad1_delta/060_90.root 90 50t 
+
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_30.root 30 50t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_50.root 50 50t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_70.root 70 50t 
+
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_10.root 20 10t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_20.root 20 20t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_30.root 20 30t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_40.root 20 40t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_50.root 20 50t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_60.root 20 60t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_70.root 20 70t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_80.root 20 80t 
+python python/fit_delta.py root/lgad1/70.root root/lgad1_delta/070_20_90.root 20 90t 
+
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_05.root 5 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_10.root 10 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_20.root 20 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_30.root 30 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_40.root 40 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_50.root 50 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_60.root 60 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_70.root 70 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_80.root 80 50t 
+python python/fit_delta.py root/lgad1/80.root root/lgad1_delta/080_90.root 90 50t 
+
+python python/fit_delta.py root/lgad1/90.root root/lgad1_delta/090_30.root 30 50t 
+python python/fit_delta.py root/lgad1/90.root root/lgad1_delta/090_50.root 50 50t 
+python python/fit_delta.py root/lgad1/90.root root/lgad1_delta/090_70.root 70 50t 
+
+
---
+++ submit.sh
@@ -0,0 +1,79 @@
+ #!/bin/bash                                                  
+                                                              
+# python python/fit_delta.py root/lgad2/50.root root/lgad2_delta/050_30.root 30 50t
+# python python/fit_delta.py root/lgad2/50.root root/lgad2_delta/050_50.root 50 50t
+# python python/fit_delta.py root/lgad2/50.root root/lgad2_delta/050_70.root 70 50t
+ 
+# python python/fit_delta.py root/lgad2/60.root root/lgad2_delta/060_30.root 30 50t
+# python python/fit_delta.py root/lgad2/60.root root/lgad2_delta/060_50.root 50 50t
+# python python/fit_delta.py root/lgad2/60.root root/lgad2_delta/060_70.root 70 50t
+                                                                             
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_05.root 5 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_10.root 10 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_20.root 20 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_30.root 30 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_40.root 40 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_50.root 50 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_60.root 60 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_70.root 70 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_80.root 80 50t
+# python python/fit_delta.py root/lgad2/70.root root/lgad2_delta/070_90.root 90 50t
+
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_20_145_2.root 20 50t
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_30_145_2.root 30 50t
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_50_145_2.root 50 50t
+python python/fit_delta.py root/lgad2/80.root root/lgad2_delta/080_70_145_2.root 70 50t
+
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_05.root 5 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_10.root 10 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_20.root 20 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_30.root 30 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_40.root 40 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_50.root 50 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_60.root 60 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_70.root 70 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_80.root 80 50t
+# python python/fit_delta.py root/lgad2/90.root root/lgad2_delta/090_90.root 90 50t
+
+# python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_20.root 20 50t
+# python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_30.root 30 50t
+# python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_50.root 50 50t
+# python python/fit_delta.py root/lgad2/100.root root/lgad2_delta/100_70.root 70 50t
+
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_05.root 5 50t
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_10.root 10 50t
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_20.root 20 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_30.root 30 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_40.root 40 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_50.root 50 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_60.root 60 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_70.root 70 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_80.root 80 50t 
+# python python/fit_delta.py root/lgad2/110.root root/lgad2_delta/110_90.root 90 50t 
+
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_05.root 5 50t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_10.root 10 50t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20.root 20 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_30.root 30 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_40.root 40 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_50.root 50 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_60.root 60 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_70.root 70 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_80.root 80 50t 
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_90.root 90 50t 
+
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_10.root 20 10t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_20.root 20 20t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_30.root 20 30t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_40.root 20 40t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_50.root 20 50t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_60.root 20 60t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_70.root 20 70t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_80.root 20 80t
+# python python/fit_delta.py root/lgad2/130.root root/lgad2_delta/130_20_90.root 20 90t
+
+
+# python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_20.root 20 50t
+# python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_30.root 30 50t
+# python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_50.root 50 50t 
+# python python/fit_delta.py root/lgad2/140.root root/lgad2_delta/140_70.root 70 50t 
